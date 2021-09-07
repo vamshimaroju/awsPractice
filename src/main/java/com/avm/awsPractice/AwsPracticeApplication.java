010cfdefb75ac2a04db494b250425561bef3e61b
Vamshi Maroju <vamsi.maroju@gmail.com>
2021-09-07 23:13:04 +0530

    committing properties

--- src/main/java/com/avm/awsPractice/AwsPracticeApplication.java
+++ src/main/java/com/avm/awsPractice/AwsPracticeApplication.java
@@ -2,8 +2,10 @@ package com.avm.awsPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.avm.*"})
 public class AwsPracticeApplication {
 
 	public static void main(String[] args) {
